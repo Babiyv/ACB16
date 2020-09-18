c3f2f7d5d555b1c6a3e1b3d7b575b495f1ecab8b
Babiyv <babiy.vladislav@gmail.com>
2020-09-18 18:02:42 +0300

    delete file from home cpu

--- src/home_work/week2/_03HomeArraysUtils/_02ArithmeticMean.java
+++ src/home_work/week2/_03HomeArraysUtils/_02ArithmeticMean.java
@@ -1,14 +1,12 @@
 package home_work.week2._03HomeArraysUtils;
 
 import utils.ArrayUtils;
-import java.util.Scanner;
 
 /*2) Найти среднее арифметическое массива*/
 public class _02ArithmeticMean {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int [] array = {1, 2, 3, 4, 0};
+        int [] array = ArrayUtils.createAndFillArray(4);
         ArrayUtils.arithmeticMeanArray(array);
 
     }
