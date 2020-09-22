e7af55f78a69b3a3740ffedc659108d9d0c0f8cf
Babiyv <babiy.vladislav@gmail.com>
2020-09-22 13:32:58 +0300

    delete file from home cpu

---
+++ src/home_work/week2/_03HomeArraysUtils/_06WhichHalfOfArrayIsBigger.java
@@ -0,0 +1,14 @@
+package home_work.week2._03HomeArraysUtils;
+
+
+import utils.ArrayUtils;
+
+/*6) Вывести в консоль элементы той половины одномерного массива у которой среднее арифметическое максимальное*/
+public class _06WhichHalfOfArrayIsBigger {
+    public static void main(String[] args) {
+
+        int [] array = {2, 2, 1, 3};
+        ArrayUtils.whichHalfOfArrayIsBigger(array);
+
+    }
+}
