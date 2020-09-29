557da985f760111a8dfd5faaab516f132e452de1
Babiyv <babiy.vladislav@gmail.com>
2020-09-29 14:35:23 +0300

    delete file from home cpu

--- src/home_work/week2/_06Extra/_04ScrollArray.java
+++ src/home_work/week2/_06Extra/_04ScrollArray.java
@@ -1,7 +1,6 @@
 package home_work.week2._06Extra;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 /*6.4 Сдвинуть массив на заданное количество позиций/
 {1,2,3,4,5} -> 2 = {4,5,1,2,3}; {1,2,3,4,5} -> 3 = {3,4,5,1,2}.
@@ -11,19 +10,19 @@ public class _04ScrollArray {
 
         int[] array = {1, 2, 3, 4, 5}; // -> {4, 5, 1, 2, 3}
         int step = -2;
-        int a, b;
+        int a;
 
         if (step > 0) {
             for (int n = 0; n < step; n++) {
                 a = array[array.length - 1];
-                for (int i = array.length - 1; i > 0; array[i] = array[i - 1], i--) ;
+                for (int i = array.length - 1; i > 0; array[i] = array[i - 1], i--);
                 array[0] = a;
             }
         }
         if (step < 0) { // <- просто захотел дополнительно попрактиковаться и сделал "прокрутку" в обратном направлении, при вводе отрицательного количества;
             for (int n = 0; n > step; n--) {
                 a = array[0];
-                for (int i = 0; i < array.length - 1; array[i] = array[i + 1], i++) ;
+                for (int i = 0; i < array.length - 1; array[i] = array[i + 1], i++);
                 array[array.length - 1] = a;
             }
         }
