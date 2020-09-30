282de9c8ab3f8ef838ed7c3e3868415ddeb153aa
Babiyv <babiy.vladislav@gmail.com>
2020-09-30 16:37:26 +0300

    delete file from home cpu

---
+++ src/home_work/week2/_08SortMethods/_01SelectSort.java
@@ -0,0 +1,21 @@
+package home_work.week2._08SortMethods;
+
+import java.util.Arrays;
+
+public class _01SelectSort {
+    public static void main(String[] args) {
+        int[] array = {2, 5, 1, 4};
+
+        for (int i = 0; i < array.length; i++){
+            for (int n = 0; n < array.length; n++){
+                if (array[n] > array[i]){
+                    int a = array[n];
+                    array[n] = array[i];
+                    array[i] = a;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+
+    }
+}
