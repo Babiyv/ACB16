4023426b3b590f4109094717fa9c849f4d7585db
Babiyv <babiy.vladislav@gmail.com>
2020-11-19 09:07:09 +0200

    delete file from home cpu

---
+++ src/home_work/week3/part1/_04UpperRegister.java
@@ -0,0 +1,32 @@
+package home_work.week3.part1;
+
+import utils.ArrayUtils;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+/*4. Верхний регистр.
+   Напишите программу, которая позволяет пользователю ввести в консоли строку, переводит первый символ слов в верхний регистр и результат выводит в консоль.
+   		Вход:
+   		иван васильевич
+
+   		Выход:
+   		Иван Васильевич*/
+public class _04UpperRegister {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        String enter = sc.nextLine();
+        System.out.println(toUpperCaseFirst(enter));
+    }
+
+    private static String toUpperCaseFirst(String incomingString) {
+        String[] split = incomingString.split(" ");
+        String answer = new String();
+        for (int i = 0; i <split.length; i++) {
+            split[i] = split[i].substring(0,1).toUpperCase() + split[i].substring(1) + " ";
+            answer += split[i];
+        }
+        return answer.trim();
+    }
+}
