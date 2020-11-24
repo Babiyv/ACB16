4f68e6f697253f5640fc0b318e8b69ccc562b6e7
Babiyv <babiy.vladislav@gmail.com>
2020-11-24 16:48:59 +0200

    delete file from home cpu

--- src/home_work/week3/part2/_01CreateMatrixAndFillFive.java
+++ src/home_work/week3/part2/_01CreateMatrixAndFillFive.java
@@ -27,7 +27,7 @@ public class _01CreateMatrixAndFillFive {
     }
 
     private static int[][] createMatrixAndFeelRandomly(int a, int b) {
-        int matrix[][] = new int[a][b];
+        int[][] matrix = new int[a][b];
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
                 matrix[i][j] = (int)(Math.random() * 100);
---
+++ src/home_work/week3/part2/_02IfEqualWriteTrue.java
@@ -0,0 +1,8 @@
+package home_work.week3.part2;
+
+/*	3.2. Создать логическую матрицу по следующим критериям, если і равно j, то элементом будет true. */
+public class _02IfEqualWriteTrue {
+    public static void main(String[] args) {
+
+    }
+}
