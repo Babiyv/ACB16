6ea3fb2f5d230dce9a96a8355b500fc692e00de8
Babiyv <babiy.vladislav@gmail.com>
2020-11-24 18:19:04 +0200

    delete file from home cpu

--- src/home_work/week3/part2/_01CreateMatrixAndFillFive.java
+++ src/home_work/week3/part2/_01CreateMatrixAndFillFive.java
@@ -28,8 +28,8 @@ public class _01CreateMatrixAndFillFive {
 
     private static int[][] createMatrixAndFeelRandomly(int a, int b) {
         int[][] matrix = new int[a][b];
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[a].length; j++) {
                 matrix[i][j] = (int)(Math.random() * 100);
             }
         }
--- src/home_work/week3/part2/_02IfEqualWriteTrue.java
+++ src/home_work/week3/part2/_02IfEqualWriteTrue.java
@@ -1,8 +1,27 @@
 package home_work.week3.part2;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /*	3.2. Создать логическую матрицу по следующим критериям, если і равно j, то элементом будет true. */
 public class _02IfEqualWriteTrue {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many lines do you want?");
+        int a = sc.nextInt();
+        System.out.println("How mane columns do you want?");
+        int b = sc.nextInt();
 
+
+        boolean[][] matrix = new boolean[a][b];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if(i == j){
+                    matrix[i][j] = true;
+                }
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
