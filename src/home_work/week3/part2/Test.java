c6c26c7fbb142752d60dfa860e3939cad791b230
Babiyv <babiy.vladislav@gmail.com>
2020-12-16 15:39:29 +0200

    delete file from home cpu

--- src/home_work/week3/part2/Test.java
+++ src/home_work/week3/part2/Test.java
@@ -1,10 +1,22 @@
 package home_work.week3.part2;
 
+import utils.ArrayUtils;
+
 public class Test {
     public static void main(String[] args) {
-        String i = "a";
-        String j = "a";
+        String input = "город";
+        String[] words = input.split(" ");
+        String[] wordsRevers = new String[words.length];
 
-        System.out.println(i == j);
+        for (int i = 0; i <words.length; i++) {
+            String[] letter = words[i].split("");
+            ArrayUtils.printArray(letter);
+            for (int j = letter.length - 1; j >= 0; j--){
+                wordsRevers[i] += letter[j];
+                ArrayUtils.printArray(wordsRevers);
+            }
+        }
+
+
     }
 }
---
+++ src/home_work/week3/part2_extra/_08DeleteRepeatedWords.java
@@ -0,0 +1,28 @@
+package home_work.week3.part2_extra;
+
+import utils.ArrayUtils;
+
+//	3.8*. Удалить из предложения слова которые повторяются.
+public class _08DeleteRepeatedWords {
+    public static void main(String[] args) {
+        String input = "One double two double three double";
+        String[]  words = input.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            String wanted = words[i];
+            int counter = 0;
+            for (int j = i + 1; j < words.length; j++) {
+                if (wanted.equalsIgnoreCase(words[j])){
+                    counter += 1;
+                    words[j] = "";
+                }
+            }
+            if (counter > 0){
+                words[i] = "";
+            }
+        }
+
+        ArrayUtils.printArray(words);
+
+    }
+}
